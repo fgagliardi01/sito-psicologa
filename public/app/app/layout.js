bc83fe8b81195d7d2611d9ff0103d6200949c120
fgagliardi01 <fefe.gagliardi01@gmail.com>
2026-02-12 12:56:37 +0100

    ripristina cartella app

---
+++ public/app/app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Dott.ssa Federica Gagliardi",
+  description: "Psicologa clinica online",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="it">
+      <body>{children}</body>
+    </html>
+  );
+}
