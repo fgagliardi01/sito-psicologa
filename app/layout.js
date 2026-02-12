7783182b1b628cfafd9036b9f3a4404c51e0486f
fgagliardi01 <fefe.gagliardi01@gmail.com>
2026-02-12 13:34:32 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,9 @@
+import "./globals.css";
+
 export const metadata = {
-  title: "Dott.ssa Federica Gagliardi",
+  title: "Dott.ssa Federica Gagliardi | Psicologa clinica online",
+  description:
+    "Psicologa clinica online. Supporto per adolescenti e adulti: ansia, difficoltà emotive e relazionali. Contatti: email e telefono.",
 };
 
 export default function RootLayout({ children }) {
