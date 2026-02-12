d1fc24cf635f29206d2458a65208525f60c8ce6c
fgagliardi01 <fefe.gagliardi01@gmail.com>
2026-02-12 13:02:18 +0100

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "Dott.ssa Federica Gagliardi",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="it">
+      <body>{children}</body>
+    </html>
+  );
+}
